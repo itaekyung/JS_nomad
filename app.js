a0dcbde750c8ce17c64bd3ae1bbf9a8dbc8b9919
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 16:17:07 +0900

    css in js

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOD");
-}
-
-h1.addEventListener("click", handleTitleClick); // 괄호를 쓰면 안돼
-h1.addEventListener("mouseenter", handleMouseEnter); // 마우스 올라갔을때
-h1.addEventListener("mouseleave", handleMouseLeave); // 마우스 떠났을 때
-window.addEventListener("resize", handleWindowResize); // 화면 크기 바뀌었을 때
-window.addEventListener("copy", handleWindowCopy); // 복사했을 때
-window.addEventListener("offline", handleWindowOffline); // 와이파이 없을 때
-window.addEventListener("online", handleWindowOnline); // 와이파이 있을 때
+h1.addEventListener("click", handleTitleClick);
