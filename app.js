ac1ef478a8f693fdee799d13e6e3eb2bc6f9d9b6
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 17:15:39 +0900

    form submission

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form Button");
 
 function onLoginBtnClick() {
-  console.log("hello", loginInput.value);
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
