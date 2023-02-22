ed06cf8b3e66f691b83022c4323d6495b0321ce1
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 15:39:26 +0900

    event2

--- app.js
+++ app.js
@@ -4,4 +4,14 @@ function handleTitleClick() {
   title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick); // 괄호를 쓰면 안돼
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
