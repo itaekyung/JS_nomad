5752fbb638f83e66920a104fda9713bb1f15f810
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 18:48:01 +0900

    Loading Username

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ const loginButton = document.querySelector("#login-form Button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //일반적으로 문자열만 포함된 변수는 대문자로 표기하고 문자열 저장에 사용
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `Hello ${username}`; // = "Hello " + username;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
