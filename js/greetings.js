0090815f918a4e76614b1a3d9819036386da55a6
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 19:53:37 +0900

    setInterval

--- js/greetings.js
+++ js/greetings.js
@@ -20,6 +20,7 @@ function paintGreetings(username) {
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
