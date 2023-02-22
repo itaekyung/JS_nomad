312a4bd909e598be36816b20a5e52310db7a3f8f
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 15:26:50 +0900

    event

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const title = document.querySelector(".hello h1");
 
-console.log(title);
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick); // 괄호를 쓰면 안돼
