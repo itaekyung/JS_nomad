e282e414a672538ae3adc9d766eaf6b1e10e85b1
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 18:06:46 +0900

    getting username

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form Button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"; //일반적으로 문자열만 포함된 변수는 대문자로 표기하고 문자열 저장에 사용
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  console.log(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
