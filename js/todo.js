c5ed6e77a68afb77fb8c8fc6fe21788254c7465d
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 23:49:45 +0900

    saving ToDo

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); // 꼭 다큐먼트 일 필요없다.
 const toDOList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
 
 function delteTodo(event) {
   const li = event.target.parentElement; //삭제하고 싶은 li 특정하기 #알아두자#
@@ -24,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
