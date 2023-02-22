ec1e097603677a10f69706f6592f9fd942a296fd
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 23:38:15 +0900

    delete To Do

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); // 꼭 다큐먼트 일 필요없다.
 const toDOList = document.getElementById("todo-list");
 
+function delteTodo(event) {
+  const li = event.target.parentElement; //삭제하고 싶은 li 특정하기 #알아두자#
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❎";
+  button.addEventListener("click", delteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+
   toDOList.appendChild(li);
 }
 
