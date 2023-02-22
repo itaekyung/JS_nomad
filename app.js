8f7fb1f8cb7e4bc05eafd13a0c7fe2b60f80b4f3
LeeTaeKyung <leek9708@gmail.com>
2023-02-22 16:06:58 +0900

    event3

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
+  h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick); // 괄호를 쓰면 안돼
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOD");
+}
+
+h1.addEventListener("click", handleTitleClick); // 괄호를 쓰면 안돼
+h1.addEventListener("mouseenter", handleMouseEnter); // 마우스 올라갔을때
+h1.addEventListener("mouseleave", handleMouseLeave); // 마우스 떠났을 때
+window.addEventListener("resize", handleWindowResize); // 화면 크기 바뀌었을 때
+window.addEventListener("copy", handleWindowCopy); // 복사했을 때
+window.addEventListener("offline", handleWindowOffline); // 와이파이 없을 때
+window.addEventListener("online", handleWindowOnline); // 와이파이 있을 때
